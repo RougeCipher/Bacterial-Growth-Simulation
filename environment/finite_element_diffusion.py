3338cb468f0db97ad015357e32f5f5fa05ff9bf7
Vishnu Rayabharapu <vishnusrmist@gmail.com>
2024-12-25 13:30:42 +0530

    Update finite_element_diffusion.py

--- environment/finite_element_diffusion.py
+++ environment/finite_element_diffusion.py
@@ -3,58 +3,57 @@ import ufl
 import math
 import numpy as np
 
+from dolfinx import fem
+from petsc4py import PETSc
+
 def solve_fem_diffusion_2d(domain, D_expr, rhs_expr, dirichlet_bcs, neumann_bcs=None):
     """
-    Solve -div(D grad(u)) = rhs_expr in domain, with advanced boundary conditions.
+    Solve -div(D grad(u)) = rhs_expr in a 2D domain with advanced boundary conditions.
     domain: a dolfinx mesh
-    D_expr: UFL expression or dolfin Constant
-    rhs_expr: UFL expression or dolfin Constant
-    dirichlet_bcs: list of (subdomain_marker, value)
-    neumann_bcs: optional list of (subdomain_marker, flux_expr)
-    returns: (FunctionSpace, solution Function)
+    D_expr: ufl expression or dolfinx.fem.Constant for diffusion
+    rhs_expr: ufl expression or dolfinx.fem.Constant for source
+    dirichlet_bcs: list of (locate_dofs, value)
+    neumann_bcs: optional list of (marker_id, flux_expr) for BCs
+    returns: (V, uh)
     """
-    V = dolfinx.fem.FunctionSpace(domain, ("CG",1))
+    V = fem.FunctionSpace(domain, ("CG", 1))
 
-    # define test/trial
+    # trial/test
     u = ufl.TrialFunction(V)
     v = ufl.TestFunction(V)
 
-    # define boundary conditions
-    # This code is just a placeholder approach for advanced subdomain usage.
-    # You can locate dofs geometrically or with subdomain markers.
-    # We'll assume user does that externally.
-    # For demonstration, let's assume "dirichlet_bcs" is a list of (locate_dofs, value).
-    # E.g. see main HPC code for how to do it with fem.locate_dofs_geometrical.
-
+    # Dirichlet BC
     bcs = []
-    for (dofs, val) in dirichlet_bcs:
-        bc = dolfinx.fem.dirichletbc(val, dofs, V)
+    for (dof_list, val) in dirichlet_bcs:
+        bc = fem.dirichletbc(val, dof_list, V)
         bcs.append(bc)
 
-    # define PDE: -div(D grad(u)) = rhs_expr
+    # PDE
     a = ufl.dot(D_expr*ufl.grad(u), ufl.grad(v))*ufl.dx
     L = rhs_expr*v*ufl.dx
 
-    # neumann BC => add boundary integrals if needed
-    # e.g. L += flux_expr * v * ufl.ds(subdomain_id)
+    # if neumann_bcs:
+    #   for (marker_id, flux_expr) in neumann_bcs:
+    #       L += flux_expr * v * ufl.ds(marker_id)
 
-    A = dolfinx.fem.petsc.assemble_matrix(dolfinx.fem.form(a), bcs=bcs)
+    a_form = fem.form(a)
+    L_form = fem.form(L)
+    A = fem.petsc.assemble_matrix(a_form, bcs=bcs)
     A.assemble()
-    b = dolfinx.fem.petsc.assemble_vector(dolfinx.fem.form(L))
-    dolfinx.fem.petsc.apply_lifting(b, [dolfinx.fem.form(a)], bcs=[bcs])
+    b = fem.petsc.assemble_vector(L_form)
+    fem.petsc.apply_lifting(b, [a_form], bcs=[bcs])
     b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-    dolfinx.fem.petsc.set_bc(b, bcs)
+    fem.petsc.set_bc(b, bcs)
 
-    # solve
-    A_mat = A
     solver = PETSc.KSP().create(domain.comm)
-    solver.setOperators(A_mat)
+    solver.setOperators(A)
     solver.setType("preonly")
     solver.getPC().setType("lu")
-    solver.solve(b, b)
+    x = b.duplicate()
+    solver.solve(b, x)
 
-    # solution
-    uh = dolfinx.fem.Function(V)
-    uh.vector.axpy(1,b)
-    uh.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-    return (V, uh)
+    uh = fem.Function(V)
+    uh.x.array[:] = x.array
+    uh.x.scatter_forward()
+    return V, uh
+
