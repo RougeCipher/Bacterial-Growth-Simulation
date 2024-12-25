9b8d71b7836fbf55f534f8e433514ea7b9278f06
Vishnu Rayabharapu <vishnusrmist@gmail.com>
2024-12-25 13:24:01 +0530

    Update environment.py

--- environment/environment.py
+++ environment/environment.py
@@ -1,10 +1,11 @@
 import numpy as np
-from environment.advanced_diffusion import adi_diffusion_2d_variableD
+from .advanced_diffusion import adi_diffusion_2d_variableD
 
 class SpatialEnvironment:
     """
-    2D environment with a nutrient field, waste field, advanced boundary conditions,
-    possibly variable diffusion.
+    2D environment storing nutrient_grid, waste_grid, etc.
+    If using ADI approach, we call adi_diffusion_2d_variableD.
+    If using FEM, see finite_element_diffusion.py.
     """
     def __init__(self, env_config):
         self.nx = env_config.grid_size_x
@@ -13,37 +14,34 @@ class SpatialEnvironment:
         self.dy = env_config.dy
 
         self.bc_type = env_config.boundary_condition_type
-        self.bc_value = env_config.boundary_value
+        self.bc_value= env_config.boundary_value
 
         self.temperature = env_config.initial_temperature
         self.pH = env_config.initial_pH
         self.oxygen_level = env_config.oxygen_level
-        self.moisture_level = env_config.moisture_level
-        self.nutrients_dict = env_config.nutrients
-        self.max_waste = env_config.max_waste
+        self.moisture_level= env_config.moisture_level
+        self.nutrients_dict= env_config.nutrients
+        self.max_waste= env_config.max_waste
 
-        if env_config.variable_diffusion and env_config.diffusion_map is not None:
+        if env_config.variable_diffusion and env_config.diffusion_map:
             self.D = np.array(env_config.diffusion_map, dtype=np.float64)
-            if self.D.shape != (self.ny, self.nx):
-                raise ValueError("diffusion_map shape mismatch with grid size.")
+            if self.D.shape!=(self.ny,self.nx):
+                raise ValueError("diffusion_map shape mismatch.")
         else:
             self.D = float(env_config.diffusion_coefficient)
 
-        self.nutrient_grid = np.zeros((self.ny, self.nx), dtype=np.float64)
+        # single nutrient field
+        self.nutrient_grid= np.zeros((self.ny,self.nx), dtype=np.float64)
+        # just take first key
         key, val = next(iter(self.nutrients_dict.items()))
-        self.nutrient_grid.fill(val)
-
-        self.waste_grid = np.zeros((self.ny, self.nx), dtype=np.float64)
+        self.nutrient_grid[:,:] = val
+        self.waste_grid = np.zeros((self.ny,self.nx), dtype=np.float64)
 
     def update_env_state(self, t, dt):
-        """
-        Dynamic environment updates for T, pH, etc.
-        """
-        self.temperature += 0.5 * np.sin(2 * np.pi * t / 24)
-        self.pH += 0.05 * np.sin(2 * np.pi * t / 24)
+        self.temperature += 0.5*np.sin(2*np.pi*t/24)
+        self.pH += 0.05*np.sin(2*np.pi*t/24)
 
-    def diffuse_nutrients(self, dt, consumption_grid):
-        # Reaction = - consumption_grid
+    def diffuse_nutrients(self, dt, consumption):
         self.nutrient_grid = adi_diffusion_2d_variableD(
             conc=self.nutrient_grid,
             D=self.D,
@@ -52,9 +50,10 @@ class SpatialEnvironment:
             dy=self.dy,
             boundary_type=self.bc_type,
             boundary_value=self.bc_value,
-            reaction=-consumption_grid
+            reaction=-consumption
         )
 
-    def accumulate_waste(self, dt, production_grid):
-        self.waste_grid += production_grid*dt
+    def accumulate_waste(self, dt, production):
+        self.waste_grid += production*dt
         np.clip(self.waste_grid, 0, self.max_waste, out=self.waste_grid)
+
